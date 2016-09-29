0451cb9a1bf5f1cde0728df3ae9582cee9f9b221
gTopa <gtopa@stuy.edu>
2016-09-29 14:05:12 -0400

    finished hw

--- 0928/app.py
+++ 0928/app.py
@@ -18,7 +18,7 @@ def disp_loginpage():
     print request.form
     return render_template("input.html")
 
-@app.route("/auth")
+@app.route("/auth", methods=['POST'])
 def disp_auth():
     print "\n\n\n"
     print ":::DIAG::: this Flask obj"
@@ -29,11 +29,12 @@ def disp_auth():
     print request.headers
     print ":::DIAG::: this request.method obj"
     print request.method
-    print ":::DIAG::: this request.args obj"
-    print request.args
     print ":::DIAG::: this request.form obj"
-    print request.form
-    return "WOOOOOOOOOO"
+    print request.form['passwd']
+    status='fail'
+    if (request.form['username']=='hi' and request.form['passwd']=="l@5a6na!"):
+        status='success'
+    return render_template("auth.html", status=status)
 
 if __name__ == "__main__":
     app.debug = True

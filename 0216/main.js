f216ef5a1b1e2f8d246d60e09cfee5d84794547c
gTopa <gtopa@stuy.edu>
2017-02-17 12:32:40 -0500

    work

--- 0216/main.js
+++ 0216/main.js
@@ -1,25 +1,98 @@
-var c=document.getElementById("slate")
-var ctx=c.getContext("2d")
-ctx.fillStyle = "#ff0000"
+//model for HTML5 canvas-based animation
 
-var animateDot = function(){
-    var x=20;
-    var drawDot = function(){
-	console.log("dot!");
-	ctx.clearRect(0,0,500,300)
+//access canvas and buttons via DOM
+var c = document.getElementById("slate");
+var stopButton = document.getElementById( "stop" );
+var circleButton = document.getElementById( "circle" );
+var dvdButton = document.getElementById( "dvd" );
+
+//prepare to interact with canvas in 2D
+var ctx = c.getContext("2d");
+
+//set fill color to lello
+ctx.fillStyle = "#ffff00";
+
+
+var requestID;
+
+
+var clear = function(e) {
+    e.preventDefault();
+    ctx.clearRect(0, 0, 500, 500);
+};
+
+
+//wrapper function will allow inner function to keep track of
+// its own complement of local variables (radius, xcor...)
+var anime = function() {
+	
+    window.cancelAnimationFrame( requestID );
+	
+    console.log(requestID);
+
+    //init params for drawing dot
+    var radius = 50;
+    var xcor = c.width / 2;
+    var grow = 1;
+
+    //Q: what happens w/ & w/o next line?
+    //window.cancelAnimationFrame( requestID );
+
+    var drawDot = function() {
+	console.log( requestID )
+
+	ctx.clearRect( 0, 0, c.width, c.height );
+	
 	ctx.beginPath();
-	ctx.arc(x%500,150,20,0,2*Math.PI);
+	ctx.arc( xcor, 150, radius, 0, 2 * Math.PI );
+	ctx.stroke();
 	ctx.fill();
-	x++;
-	rid=window.requestAnimationFrame(drawDot);
+	if(radius==c.height/2||radius==0){
+	    grow=grow*-1;
+	};
+	radius+=grow;
+	requestID = window.requestAnimationFrame( drawDot );
     };
     drawDot();
 };
-var stopIt = function(){
-    window.cancelAnimationFrame(rid)
+
+var bounce = function() {
+	
+    window.cancelAnimationFrame( requestID );
+	
+    console.log(requestID);
+
+    //init params for drawing square
+    var ycor = (Math.random()*(c.height-49))+50;
+    var xcor = (Math.random()*(c.width-49))+49;
+    var xdir = 1;
+    var ydir = -1;
+
+    //Q: what happens w/ & w/o next line?
+    //window.cancelAnimationFrame( requestID );
+
+    var dvd = function() {
+	console.log( requestID )
+
+	ctx.clearRect( 0, 0, c.width, c.height );
+	
+	ctx.beginPath();
+	ctx.fillRect(xcor, ycor, 50,50);
+	ctx.stroke();
+	ctx.fill();
+	requestID = window.requestAnimationFrame( dvd );
+    };
+    dvd();
 };
 
-var sb=document.getElementById("stop");
-sb.addEventListener("click", stopIt);
-c.addEventListener("click", animateDot);
+var stopIt = function() {
+    console.log( requestID );
+    window.cancelAnimationFrame( requestID );
+};
 
+
+circleButton.addEventListener( "click", anime )
+
+stopButton.addEventListener( "click",  stopIt );
+
+dvdButton.addEventListener( "click", bounce )
